397a69d04fa828cb1df6b0b0da55bebb8d7f1691
Renne Rocha <me@rennerocha.com>
2012-01-10 17:18:36 -0200

    Alterando o estilo da formatação de código

--- _filters/syntax_highlight.py
+++ _filters/syntax_highlight.py
@@ -10,8 +10,8 @@ config = {"name": "Syntax Highlighter",
           "description": "Highlights blocks of code based on syntax",
           "author": "Ryan McGuire",
           "css_dir": "/css",
-          "preload_styles": []}
-
+          "style": "monokai",
+          "preload_styles": ['monokai']}
 
 def init():
     #This filter normally only loads pygments styles when needed.
